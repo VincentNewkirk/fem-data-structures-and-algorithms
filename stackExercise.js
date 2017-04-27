7f88c1e5cefd080544c6675166526a29f1282968
Vincent <vincentnewkirk@gmail.com>
2017-04-27 22:28:34 +0900

    created stack exercise and file

---
+++ stackExercise.js
@@ -0,0 +1,19 @@
+var Stack = function() {
+  this.storage = '';
+};
+
+Stack.prototype.push = function(val) {
+
+};
+
+Stack.prototype.pop = function() {
+
+};
+
+Stack.prototype.size = function() {
+
+};
+
+var myWeeklyMenu = new Stack();
+
+myWeeklyMenu.push('RedBeans');
