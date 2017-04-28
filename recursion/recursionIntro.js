84f65ee4e1cfe98997278a3cd80115bc627b56b0
Vincent <vincentnewkirk@gmail.com>
2017-04-28 20:59:28 +0900

    completed second problem

--- recursion/recursionIntro.js
+++ recursion/recursionIntro.js
@@ -3,10 +3,16 @@ const numberOne = (n) => {
   while (n !== 0) {
     n -= 1;
   }
-  return n;
+  return 'success';
 };
 
 //2. Next, try looping just like above except using recursion
+const numberTwo = (n) => {
+  if (n === 0) {
+    return 'success';
+  }
+  return numberTwo(n - 1);
+};
 
 //3.Write a function 'exponent' that takes two arguments base, and expo, uses a while loop to return the exponenet value of the base.
 
