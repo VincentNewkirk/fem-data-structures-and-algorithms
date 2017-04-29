780e1c3339889463a41fd4f2ab61b807d49839ad
Vincent <vincentnewkirk@gmail.com>
2017-04-29 14:03:40 +0900

    solved factorial exercise

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -3,3 +3,13 @@ Implement factorial.
 
 factorial(5) => 5*4*3*2*1 => 120
 */
+
+const factorial = (num) => {
+  if (num === 1) {
+    return num;
+  }
+  return num * factorial(num - 1);
+};
+
+// console.log(factorial(5));
+//120
