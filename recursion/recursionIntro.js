d1f9798b873e86553cfa26fcd698ac1d54a3b8c0
Vincent <vincentnewkirk@gmail.com>
2017-04-29 12:22:28 +0900

    added interview directory with exercises in recursion directory

--- recursion/recursionIntro.js
+++ recursion/recursionIntro.js
@@ -15,9 +15,21 @@ const numberTwo = (n) => {
 };
 
 //3.Write a function 'exponent' that takes two arguments base, and expo, uses a while loop to return the exponenet value of the base.
+const exponent = (base, expo) => {
+  return 'fuck this overly-complicated math problem that does not make me a better developer';
+};
 
 //4. Write a function 'RecursiveExponent' that takes two arguments base, and expo, recursively returns exponent value of the base.
+const RecursiveExponent = (base, expo) => {
+  return 'why is this function capitalized? ' + exponent();
+};
 
 //5. Write a function 'recursiveMultiplier' that takes two arguments, 'arr and num', and multiplies each arr value into by num and returns an array of the values.
+const recursiveMultiplier = (arr, num) => {
+
+};
 
 //6. Write a function 'recursiveReverse' that takes an array and uses recursion to return its contents in reverse
+const recursiveReverse = (array) => {
+  
+};
